2604c9e1ec7c57301d67798e8c3ea1d7611c19d2
Dustin Apodaca <dustinapodaca@gmail.com>
2023-03-25 18:19:18 -0600

    scaffolding ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#2E63F6",
+          "secondary": "#555B67",
+          "accent": "#F3F5F9",
+          "neutral": "#0F1C35",
+          "base-100": "#F5F6FA",
+          "info": "#0000FF",
+          "success": "#5EB182",
+          "warning": "#FFFF00",
+          "error": "#E30A17",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 }
